84c50265a6a612b503f9909289d892afe5dfca27
Divyansh Tripathi <47769848+Divyansht85@users.noreply.github.com>
2024-07-17 00:22:00 +0530

    Added Kubernetes files and disabled google signin button

--- app/login/page.js
+++ app/login/page.js
@@ -40,7 +40,7 @@ const Login = () => {
       <div className="social-login-buttons">
         <div className="flex flex-col gap-2 p-10 items-center">
           <button
-            onClick={() => handleSignIn("google")}
+            // onClick={() => handleSignIn("google")}
             className="flex items-center w-64 bg-slate-50 text-black border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
           >
             <svg
